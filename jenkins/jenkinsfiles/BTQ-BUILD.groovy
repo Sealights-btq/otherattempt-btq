8dd0564ff36ef2c2c979637517287470ab4773e1
sldevopsd <30296406+sldevopsd@users.noreply.github.com>
2025-05-15 13:51:02 +0300

    Initial commit

---
+++ jenkins/jenkinsfiles/BTQ-BUILD.groovy
@@ -0,0 +1,60 @@
+pipeline{
+  agent {
+    kubernetes {
+      yaml readTrusted('jenkins/pod-templates/BTQ_BUILD_shell_kaniko_pod.yaml')
+      defaultContainer "shell"
+    }
+  }
+
+  options {
+    buildDiscarder logRotator(numToKeepStr: '100')
+  }
+  parameters {
+    string(name: 'TAG', defaultValue: '1.2.2', description: 'latest tag')
+    string(name: 'BRANCH', defaultValue: 'main', description: 'default branch')
+    choice(name: 'SERVICE', choices: ["adservice","cartservice","checkoutservice", "currencyservice","emailservice","frontend","paymentservice","productcatalogservice","recommendationservice","shippingservice","sealightsservice"], description: 'Service name to build')
+    string(name: 'BUILD_NAME', defaultValue: 'none', description: 'build name')
+  }
+  environment{
+    ECR_FULL_NAME = "btq-${params.SERVICE}"
+    ECR_URI = "474620256508.dkr.ecr.eu-west-1.amazonaws.com/${env.ECR_FULL_NAME}"
+    SL_TOKEN = (sh(returnStdout: true, script:"aws secretsmanager get-secret-value --region eu-west-1 --secret-id 'btq/template_token' | jq -r '.SecretString' | jq -r '.template_token'" )).trim()
+    TAG = "template_${params.TAG}"
+  }
+
+  stages{
+    stage('Init') {
+      steps {
+        script {
+          // Clone the repository with the specified branch.
+          git branch: params.BRANCH, url: 'https://github.com/Sealights-btq/template-btq.git'
+          currentBuild.displayName = "${SERVICE}-${BUILD_NAME}"
+          stage("Build Docker ${params.SERVICE} Image") {
+            container(name: 'kaniko'){
+              script {
+                def CONTEXT = params.SERVICE == "cartservice" ? "./src/${params.SERVICE}/src" : "./src/${params.SERVICE}"
+                def DP = "${CONTEXT}/Dockerfile"
+                def D = "${env.ECR_URI}:${env.TAG}"
+                def BRANCH = params.BRANCH
+                def BUILD_NAME = "${params.BUILD_NAME}"
+                def SL_TOKEN = env.SL_TOKEN
+                def AGENT_URL = params.AGENT_URL
+                def AGENT_URL_SLCI = params.AGENT_URL_SLCI
+
+                sh """
+                    /kaniko/executor \
+                    --context ${CONTEXT} \
+                    --dockerfile ${DP} \
+                    --destination ${D} \
+                    --build-arg BRANCH=${BRANCH} \
+                    --build-arg BUILD_NAME=${BUILD_NAME} \
+                    --build-arg SEALIGHTS_TOKEN=${SL_TOKEN}
+                """
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
---
+++ jenkins/jenkinsfiles/deploy-btq.groovy
@@ -0,0 +1,67 @@
+properties([
+        parameters([
+                string(name: 'tag', defaultValue: 'null', description: 'change btq version'),
+                string(name: 'buildname', defaultValue: 'null', description: 'change sealights buildname for cd agents (if you change anything then this parameter is mandatory)'),
+                string(name: 'labid', defaultValue: 'null', description: 'change sealights lab_id'),
+                string(name: 'branch', defaultValue: 'null', description: 'change branch to pull (effects tests and helm)')
+        ])
+])
+
+pipeline {
+   agent {
+        kubernetes {
+            defaultContainer 'shell'
+            yaml """
+                apiVersion: v1
+                kind: Pod
+                metadata:
+                  labels:
+                    some-label: some-value
+                spec:
+                  containers:
+                  - name: shell
+                    image: "public.ecr.aws/a2q7i5i2/sl-jenkins-base-ci:latest"
+                    command:
+                    - cat
+                    tty: true
+                    env:
+                      - name: AWS_ACCESS_KEY_ID
+                        valueFrom:
+                          secretKeyRef:
+                            name: aws-secret
+                            key: AWS_ACCESS_KEY_ID
+                      - name: AWS_SECRET_ACCESS_KEY
+                        valueFrom:
+                          secretKeyRef:
+                            name: aws-secret
+                            key: AWS_SECRET_ACCESS_KEY
+            """
+        }
+    }
+    environment {
+        SL_TOKEN = (sh(returnStdout: true, script:"aws secretsmanager get-secret-value --region eu-west-1 --secret-id 'btq/template_token' | jq -r '.SecretString' | jq -r '.template_token'" )).trim()
+        IDENTIFIER = 'btq-template.btq.sealights.co'
+        tag = "template_${params.tag}"
+    }
+     stages {
+        stage("Preparing Spin up") {
+            steps {
+                script {
+                    cleanWs()
+                    ENV_NAME = "${IDENTIFIER}"
+                    currentBuild.displayName = "${ENV_NAME} btq update"
+                    LOWER_ENV_NAME = "${ENV_NAME}".toLowerCase()
+                    IP = "${IDENTIFIER}"
+                            stage("Updating Helm") {
+                                sh script: """
+                                    aws secretsmanager get-secret-value --region eu-west-1 --secret-id 'btq/template_key_pair' | jq -r '.SecretString' | jq -r '.template_key_pair' > key.pem
+                                    chmod 0400 key.pem
+
+                                    ssh -o StrictHostKeyChecking=no -i key.pem ec2-user@internal-template.btq.sealights.co 'bash /opt/sealights/install-btq.sh --tag=${env.tag} --buildname=${params.buildname} --labid=${params.labid} --branch=${params.branch} --token=${env.SL_TOKEN} --sl_branch=${params.branch}'
+                                """
+                            }
+                }
+            }
+        }
+    }
+}
